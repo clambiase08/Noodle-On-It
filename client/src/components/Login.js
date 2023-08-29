81be312a123d6f6615d26000f5b8caa039f636b8
Jonathan <jpreuven@gmail.com>
2023-08-29 12:29:15 -0700

    almost done with login and signup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,25 @@
 import React, { useEffect, useState } from "react";
-// import { Link, NavLink } from "react-router-dom";
+// import { Box, Button } from "@chakra-ui/react";
+// import { Input as ChakraInput } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+// import { Formik, FormikProps } from "formik";
+// import * as yup from "yup";
+
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+// import Input from "./Main/Input.js";
 
 export default function Login({
   user,
@@ -14,8 +33,13 @@ export default function Login({
 
   const history = useHistory();
 
+  function handleSignUp(e) {
+    history.push("/signup");
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+    // console.log(e);
     fetch("/login", {
       method: "POST",
       headers: {
@@ -33,19 +57,174 @@ export default function Login({
     });
   }
 
+  // return (
+  //   <Box p={40} mt="20">
+  //     <form onSubmit={(e) => handleSubmit(e)}>
+  //       <ChakraInput
+  //         htmlSize={15}
+  //         width="auto"
+  //         type="text"
+  //         value={username}
+  //         onChange={(e) => setUsername(e.target.value)}
+  //       />
+  //       {/* <Input
+  //         // type="text"
+  //         // value={username}
+  //         username="username"
+  //         onChange={(e) => setUsername(e.target.value)}
+  //       /> */}
+  //       <br />
+  //       <ChakraInput
+  //         htmlSize={15}
+  //         width="auto"
+  //         type="password"
+  //         value={password}
+  //         onChange={(e) => setPassword(e.target.value)}
+  //       />
+  //       <Button type="submit">Login</Button>
+  //     </form>
+  //   </Box>
+  // );
+  // //   return (
+  // //     <Box p={24} mt="20">
+  // //       <Formik
+  // //         initialValues={{ username: "", email: "", password: "" }}
+  // //         validationSchema={formSchema}
+  // //         onSubmit={handleSubmit}
+  // //       >
+  // //         {(props) => (
+  // //           <form>
+  // //             <Input
+  // //               name="username"
+  // //               value={username}
+  // //               onChange={Formik.handleChange}
+  // //             />
+  // //             <Input
+  // //               type="password"
+  // //               value={password}
+  // //               name="password"
+  // //               onChange={Formik.handleChange}
+  // //             />
+  // //             <button type="submit">Login</button>
+  // //           </form>
+  // //         )}
+  // //       </Formik>
+  // //     </Box>
+  // //   );
+  // }
+
+  // 'use client'
+
+  // import {
+  //   Flex,
+  //   Box,
+  //   FormControl,
+  //   FormLabel,
+  //   Input,
+  //   Checkbox,
+  //   Stack,
+  //   Button,
+  //   Heading,
+  //   Text,
+  //   useColorModeValue,
+  // } from '@chakra-ui/react'
+
+  // export default function SimpleCard() {
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to enjoy all of our cool features ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Username</FormLabel>
+                <ChakraInput
+                  htmlSize={35}
+                  width="auto"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <ChakraInput
+                  htmlSize={35}
+                  width="auto"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: "column", sm: "row" }}
+                  align={"start"}
+                  justify={"space-between"}
+                >
+                  {/* <Checkbox>Remember me</Checkbox> */}
+                  <Text
+                    color={"blue.400"}
+                    _hover={{
+                      color: "blue.600",
+                    }}
+                    onClick={handleSignUp}
+                  >
+                    Not a member? Sign up now!
+                  </Text>
+                </Stack>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  type="submit"
+                >
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+    </Flex>
   );
+}
+{
+  /* <form onSubmit={(e) => handleSubmit(e)}>
+  <ChakraInput
+    htmlSize={15}
+    width="auto"
+    type="text"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+  />
+
+  <br />
+  <ChakraInput
+    htmlSize={15}
+    width="auto"
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+  />
+  <Button type="submit">Login</Button>
+</form>; */
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,4 +1,20 @@
 import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Flex,
+  Stack,
+  Heading,
+  Text,
+  Container,
+  Input as ChakraInput,
+  Button,
+  SimpleGrid,
+  Avatar,
+  AvatarGroup,
+  useBreakpointValue,
+  IconProps,
+  Icon,
+} from "@chakra-ui/react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Signup({ user, setUser, fetchUser }) {
@@ -7,6 +23,10 @@ export default function Signup({ user, setUser, fetchUser }) {
   const [username, setUsername] = useState("");
 
   const history = useHistory();
+
+  function handleLogin(e) {
+    history.push("/login");
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -26,27 +46,187 @@ export default function Signup({ user, setUser, fetchUser }) {
     });
   }
 
+  // const Blur = (props: IconProps) => {
+  const Blur = (props) => {
+    return (
+      <Icon
+        width={useBreakpointValue({ base: "100%", md: "40vw", lg: "30vw" })}
+        zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
+        height="560px"
+        viewBox="0 0 528 560"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <circle cx="71" cy="61" r="111" fill="#F56565" />
+        <circle cx="244" cy="106" r="139" fill="#ED64A6" />
+        <circle cy="291" r="139" fill="#ED64A6" />
+        <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
+        <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
+        <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
+        <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+      </Icon>
+    );
+  };
+
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input
-        type="text"
-        value={username}
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
+    <Box position={"relative"}>
+      <Container
+        as={SimpleGrid}
+        maxW={"7xl"}
+        columns={{ base: 1, md: 2 }}
+        spacing={{ base: 10, lg: 32 }}
+        py={{ base: 10, sm: 20, lg: 32 }}
+      >
+        <Stack spacing={{ base: 10, md: 20 }}>
+          <Heading
+            lineHeight={1.1}
+            fontSize={{ base: "3xl", sm: "4xl", md: "5xl", lg: "6xl" }}
+          >
+            Welcome to{" "}
+            <Text
+              as={"span"}
+              bgGradient="linear(to-r, red.400,pink.400)"
+              bgClip="text"
+            >
+              Noodle-On-It
+            </Text>{" "}
+          </Heading>
+        </Stack>
+        <Stack
+          bg={"gray.50"}
+          rounded={"xl"}
+          p={{ base: 4, sm: 6, md: 8 }}
+          spacing={{ base: 8 }}
+          maxW={{ lg: "lg" }}
+        >
+          <Stack spacing={4}>
+            <Heading
+              color={"gray.800"}
+              lineHeight={1.1}
+              fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+            >
+              Join the Noodle Community
+              <Text
+                as={"span"}
+                bgGradient="linear(to-r, red.400,pink.400)"
+                bgClip="text"
+              >
+                !
+              </Text>
+            </Heading>
+            {/* <Text color={"gray.500"} fontSize={{ base: "sm", sm: "md" }}>
+              We’re looking for amazing engineers just like you! Become a part
+              of our rockstar engineering team and skyrocket your career!
+            </Text> */}
+          </Stack>
+          <Box as={"form"} onSubmit={(e) => handleSubmit(e)} mt={10}>
+            {/* <form onSubmit={(e) => handleSubmit(e)}> */}
+            <Stack spacing={4}>
+              <ChakraInput
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+                type="text"
+                value={username}
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <ChakraInput
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+                type="password"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <ChakraInput
+                bg={"gray.100"}
+                border={0}
+                color={"gray.500"}
+                _placeholder={{
+                  color: "gray.500",
+                }}
+                type="email"
+                value={email}
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {/* <Button
+                  fontFamily={"heading"}
+                  bg={"gray.200"}
+                  color={"gray.800"}
+                  type="submit"
+                >
+                  Join
+                </Button> */}
+            </Stack>
+            <Button
+              fontFamily={"heading"}
+              mt={8}
+              w={"full"}
+              bgGradient="linear(to-r, red.400,pink.400)"
+              color={"white"}
+              _hover={{
+                bgGradient: "linear(to-r, red.400,pink.400)",
+                boxShadow: "xl",
+              }}
+              type="submit"
+            >
+              Submit
+            </Button>
+            {/* </form> */}
+            <Text
+              color={"blue.400"}
+              _hover={{
+                color: "blue.600",
+              }}
+              onClick={handleLogin}
+            >
+              Already have an account? Login now!
+            </Text>
+          </Box>
+          form
+        </Stack>
+      </Container>
+      <Blur
+        position={"absolute"}
+        top={-10}
+        left={-10}
+        style={{ filter: "blur(70px)" }}
       />
-      <input
-        type="password"
-        value={password}
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="email"
-        value={email}
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <button type="submit">Signup</button>
-    </form>
+    </Box>
   );
 }
+
+// return (
+//   <form onSubmit={(e) => handleSubmit(e)}>
+//     <input
+//       type="text"
+//       value={username}
+//       placeholder="Username"
+//       onChange={(e) => setUsername(e.target.value)}
+//     />
+//     <input
+//       type="password"
+//       value={password}
+//       placeholder="Password"
+//       onChange={(e) => setPassword(e.target.value)}
+//     />
+//     <input
+//       type="email"
+//       value={email}
+//       placeholder="Email"
+//       onChange={(e) => setEmail(e.target.value)}
+//     />
+//     <button type="submit">Signup</button>
+//   </form>
+// );
+// 'use client'
